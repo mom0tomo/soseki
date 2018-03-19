3614aa9cb589bb7f465344d8b2f8fd2d23846811
mom0tomo <momo76w@gmail.com>
2018-03-20 01:36:52 +0900

    wip

--- app/controllers/proverb_controller.rb
+++ app/controllers/proverb_controller.rb
@@ -1,4 +1,14 @@
+require 'net/http'
+
 class ProverbController < ApplicationController
   def show
+  	uri = URI.parse("http://localhost:3002/proverbs")
+  	@query = uri.query
+
+  	response = Net::HTTP.start(uri.host, uri.port) do |http|
+  		http.open_timeout = 5
+  		http.read_timeout = 10
+  		http.get(uri.request_uri)
+  	end	
   end
 end
