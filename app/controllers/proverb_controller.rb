8ee4ba193002eb6d00236dd53cf20e4fc1f2e060
mom0tomo <momo76w@gmail.com>
2018-03-23 18:46:06 +0900

    雑に出す

--- app/controllers/proverb_controller.rb
+++ app/controllers/proverb_controller.rb
@@ -2,13 +2,7 @@ require 'net/http'
 
 class ProverbController < ApplicationController
   def show
-  	uri = URI.parse("http://localhost:3002/proverbs")
-  	@query = uri.query
-
-  	response = Net::HTTP.start(uri.host, uri.port) do |http|
-  		http.open_timeout = 5
-  		http.read_timeout = 10
-  		http.get(uri.request_uri)
-  	end	
+  	@proverbs = Proverb.new
+    @proverb = @proverbs.todays_proverb
   end
 end
