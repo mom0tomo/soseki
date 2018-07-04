7ebb44992c5358d2037436eef7e85ca354253b24
mom0tomo <momo76w@gmail.com>
2018-07-04 11:27:59 +0900

    rename to plural

--- app/controllers/proverbs_controller.rb
+++ app/controllers/proverbs_controller.rb
@@ -1,4 +1,8 @@
-class ProverbController < ApplicationController
+class ProverbsController < ApplicationController
+  def index
+
+  end
+
   def show
 
   end
--- spec/controllers/proverbs_controller_spec.rb
+++ spec/controllers/proverbs_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ProverbController, type: :controller do
+RSpec.describe ProverbsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
