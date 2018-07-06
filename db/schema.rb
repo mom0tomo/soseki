d863397a740433a87da5068a64ce9050f4338ae8
mom0tomo <momo76w@gmail.com>
2018-07-06 11:38:06 +0900

    create user_proverbs_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180703071845) do
+ActiveRecord::Schema.define(version: 20180706023407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20180703071845) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_proverbs_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "proverb_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["proverb_id"], name: "index_user_proverbs_users_on_proverb_id"
+    t.index ["user_id"], name: "index_user_proverbs_users_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
@@ -44,4 +53,6 @@ ActiveRecord::Schema.define(version: 20180703071845) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_proverbs_users", "proverbs"
+  add_foreign_key "user_proverbs_users", "users"
 end
