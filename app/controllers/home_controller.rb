9c043e1e312d11b5b42ca713fdc1553f20fb603e
mom0tomo <momo76w@gmail.com>
2018-07-14 14:26:21 +0900

    refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
-    @proverb = Proverb.new
+    @proverb = random_pickuped_proverb
+  end
+
+  private
+
+  def random_pickuped_proverb
+    proverbs_hash = get_and_parse_json
+    proverbs_hash.sample['proverb']
+  end
+
+  def get_and_parse_json
+    uri = URI.parse('http://localhost:3002/proverbs')
+    json = Net::HTTP.get(uri)
+    JSON.parse(json)
   end
 end
--- app/models/proverb.rb
+++ app/models/proverb.rb
@@ -2,5 +2,4 @@ class Proverb < ApplicationRecord
   has_many :user_proverbs_users,
            foreign_key: :user_id,
            dependent: :destroy
-  has_many :users
 end
